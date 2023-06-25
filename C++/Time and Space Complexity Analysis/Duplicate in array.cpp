7229a42b981b33f382b237f10b7180e8617ee465
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-06-25 17:50:27 +0530

    Updated file
    reduced TC from O(n)->O(1)

--- C++/Time and Space Complexity Analysis/Duplicate in array.cpp
+++ C++/Time and Space Complexity Analysis/Duplicate in array.cpp
@@ -10,19 +10,20 @@ int findDuplicate_sort(int *arr, int n) {
   }
 }
 
+
+//time : 0(n), space: O(1)
 int findDuplicate_sum(int *arr, int n) {
     int sumOfArray = accumulate(arr, arr+n, 0);
-    int sumOfNum = 0;
+    int sumOfNum = (n-2)*(n-1)/2;
     // ---- usd accumulate instead of below
     // for ( int i = 0 ; i < n ; i++){
     //     sum+=arr[i];
     // }
-    for (int i = 0; i < n-1; i++) {
-      sumOfNum+= i ;
-        }
+    // for (int i = 0; i < n-1; i++) {
+    //   sumOfNum+= i ;
+    //     }
     
     return sumOfArray-sumOfNum;
-
 }
 
 
