fdcd595cd20a89af4e3631908deab7744332506b
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-06-30 23:32:59 +0530

    File added

---
+++ C++/OOPS/Complex number class.cpp
@@ -0,0 +1,26 @@
+class ComplexNumbers {
+        public:
+        int real;
+        int imaginary;
+                    
+        ComplexNumbers(int real,int imaginary){
+         this->real=real;
+         this->imaginary=imaginary;
+                                            }
+        void print(){
+       cout<<this->real<<" + i"<<this->imaginary<<endl;
+                                                                }
+                                                                    
+        void plus(ComplexNumbers const &c2){
+            this->real=real+c2.real;
+            this->imaginary=this->imaginary+c2.imaginary;
+                                                                                            }
+                                                                                                
+        void multiply(ComplexNumbers const &c2){
+            int real=this->real*c2.real-(this->imaginary*c2.imaginary);
+            int imaginary=(this->real*c2.imaginary + this->imaginary*c2.real);
+            this->real=real;
+            this->imaginary=imaginary;
+                    }
+                                                                                                                                            
+};
