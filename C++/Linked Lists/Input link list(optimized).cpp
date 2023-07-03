a2ce04b225aa27255775ad91a9c66c8b8738dc5f
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-07-04 00:34:56 +0530

    Added file

---
+++ C++/Linked Lists/Input link list(optimized).cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    
+    Node(int data){
+        this->data=data;
+        next=NULL;
+    }
+};
+
+    Node* takeInput(){
+        int data;
+        cin>>data;
+        Node* head = NULL;
+        Node* tail = NULL;
+        while(data!=-1){
+            Node* newNode= new Node(data);
+            if(head==NULL){
+                head=newNode;
+                tail=newNode;
+            }
+            else{
+                tail->next=newNode;
+                tail=tail->next;
+                }
+            
+            cin>>data;
+        }
+        return head;
+    }
+    
+    void print(Node* head){
+        while( head!=NULL){
+            cout<< head->data<<" ";
+            head=head->next;
+        }
+    }
+    
+    
+int main(){
+    Node* head = takeInput();
+    print(head);
+}
