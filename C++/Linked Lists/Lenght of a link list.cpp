c17d5139fbc20b2b100a167d4c87b13ad36c5e12
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-07-04 01:04:02 +0530

    Added file

--- C++/Linked Lists/Lenght of a link list.cpp
+++ C++/Linked Lists/Lenght of a link list.cpp
@@ -1,9 +1,31 @@
-int length(Node *head)
+// Recursive approach 
+// time complexity O(n) space complexity O(m)
+int length_rec(Node *head){
+    if(head==NULL){
+        return 0;
+    }
+    return 1 + length_rec(head->next);
+
+}
+
+//Iterative approach
+// time complexity O(n) , space complexity O(1)
+int length_iterative(Node *head)
 {
-    int length=0;
-	    while(head!=NULL){
-		    length++;
-			head=head->next;
-						    }
-	return length;
+    Node* temp = head;
+    int length = 0 ;
+    while(temp!=NULL){
+        if(temp!=NULL){
+            length+=1;
+            temp = temp->next;
+
+        }   
+    }
+    return length;
+}
+
+
+int length(Node *head) {
+  return length_rec(head);
+//   return length_iterative(head);
 }
