5188e5d7a615e9a90f377ee2d5579fc9a012d0be
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-07-12 18:06:57 +0530

    Added file

---
+++ C++/Searching and Sorting/Merge sort.cpp
@@ -0,0 +1,34 @@
+// Time complexity =O(nlogn) , space O(n)
+void merge2array(vector<int>&arr,int s , int e){
+    vector<int>output(e-s+1);
+    
+    int mid  = (s+e)/2;
+    int i = s , j = mid+1 , k=0;
+    while (i<=mid && j<= e){
+        if( arr[i]<arr[j]){
+            output[k++]=arr[i++];
+        }
+        else{
+            output[k++]=arr[j++];
+        }
+    }
+    while ( i<=mid){
+        output[k++]=arr[i++];
+    }
+    while ( j<=e){
+        output[k++]=arr[j++];
+        }
+        for (int x = s; x <= e; x++) {
+          arr[x] = output[x - s];
+        }
+}
+
+void mergeSort(vector<int>& arr, int s, int e) {  //3        
+    if (s>=e) {
+        return ;
+    }
+    int mid = (s+e)/2;
+    mergeSort(arr,s, mid);
+    mergeSort(arr,mid+1, e);  
+    merge2array(arr,  s, e);
+}
