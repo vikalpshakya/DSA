09b378262f7d6b472e96c32074856f17b9756a5f
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-07-15 02:22:18 +0530

    Added file

--- C++/Linked Lists/Merge Sort.cpp
+++ C++/Linked Lists/Merge Sort.cpp
@@ -1,20 +1,18 @@
 //   Time complexity = O(n*log(n)) , Space complexity O(n)  
 
 Node* mergeTwoLists(Node* list1, Node* list2) {
-		if (list1 == NULL) {
-				return list2;
-		}
-		if (list2 == NULL) {
-				return list1;
-		}
+		if (list1 == NULL) return list2;
+		
+		if (list2 == NULL) return list1;
+
 		if (list1->data < list2->data) {
-				list1->next = mergeTwoLists(list1->next, list2);
-				return list1;
+			list1->next = mergeTwoLists(list1->next, list2);
+			return list1;
 		} 
 		else {
-				list2->next = mergeTwoLists(list1, list2->next);
-				return list2;
-}
+			list2->next = mergeTwoLists(list1, list2->next);
+			return list2;
+	}
 												}
 
 Node* findMidPoint(Node* head) {
