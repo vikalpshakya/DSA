27e2510aa7c05be26bb0cc8ab9d20442dbb12206
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-07-18 17:01:10 +0530

    Added file

---
+++ C++/Binary Trees/Counting number of nodes.cpp
@@ -0,0 +1,26 @@
+//     Time complexity: O(N)
+//     Space complexity: O(N)
+
+int numNodes(BinaryTreeNode<int>* root) {
+  if ( root == NULL ) return 0;
+  return 1 + numNodes(root->left) + numNodes(root->right) ;
+
+
+
+/**********************************************************
+	Following is the Binary Tree Node class structure
+
+	template <typename T>
+	class BinaryTreeNode {
+    	public : 
+    	T data;
+    	BinaryTreeNode<T> *left;
+    	BinaryTreeNode<T> *right;
+
+    	BinaryTreeNode(T data) {
+        	this -> data = data;
+        	left = NULL;
+        	right = NULL;
+    	}
+	};
+***********************************************************/
