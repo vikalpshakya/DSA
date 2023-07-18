1251bf5a1a3868a9bc49125be65a886b219d5596
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-07-18 18:05:51 +0530

    Added file

---
+++ C++/Binary Trees/Preorder Binary Tree.cpp
@@ -0,0 +1,30 @@
+//    Time complexity: O(N)
+//    Space complexity: O(H)
+
+
+void preOrder(BinaryTreeNode<int> *root) {
+	if ( !root) return;  // base case 
+  
+	cout << root->data <<" ";
+	preOrder(root->left ) ;
+	preOrder(root->right);
+  
+}
+
+/**********************************************************
+	Following is the Binary Tree Node class structure
+
+	template <typename T>
+	class BinaryTreeNode {
+    	public : 
+    	T data;
+    	BinaryTreeNode<T> *left;
+    	BinaryTreeNode<T> *right;
+
+    	BinaryTreeNode(T data) {
+        	this -> data = data;
+        	left = NULL;
+        	right = NULL;
+    	}
+	};
+***********************************************************/
