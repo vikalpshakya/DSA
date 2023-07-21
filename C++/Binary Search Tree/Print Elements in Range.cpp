6969e661e772f370089bd82c6667fb75f400f075
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-07-21 20:38:06 +0530

    Added file

---
+++ C++/Binary Search Tree/Print Elements in Range.cpp
@@ -0,0 +1,23 @@
+/*
+    Time complexity: O(N)
+    Space complexity: O(H)
+
+    where N is the number of nodes in the input BST and 
+    H is the height of the input BST
+*/
+
+//     Given a Binary Search Tree and two integers k1 and k2, find and print the elements which are in range k1 and k2 (both inclusive).
+//     Print the elements in increasing order.
+
+
+void elementsInRangeK1K2(BinaryTreeNode<int>* root, int k1, int k2) {
+	if (!root) return;
+		// In-order traversal
+	elementsInRangeK1K2(root->left, k1, k2);
+	if (root->data >= k1 &&
+		root->data <= k2) {
+		cout << root->data << " ";
+	}
+	elementsInRangeK1K2(root->right, k1,k2);
+}
+        
