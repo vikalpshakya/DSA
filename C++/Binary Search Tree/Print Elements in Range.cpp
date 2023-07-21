841ae86609be5bce08dcf6aca3b03acb8417233b
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-07-21 20:38:52 +0530

    Added file

--- C++/Binary Search Tree/Print Elements in Range.cpp
+++ C++/Binary Search Tree/Print Elements in Range.cpp
@@ -14,8 +14,7 @@ void elementsInRangeK1K2(BinaryTreeNode<int>* root, int k1, int k2) {
 	if (!root) return;
 		// In-order traversal
 	elementsInRangeK1K2(root->left, k1, k2);
-	if (root->data >= k1 &&
-		root->data <= k2) {
+	if (root->data >= k1 && root->data <= k2) {
 		cout << root->data << " ";
 	}
 	elementsInRangeK1K2(root->right, k1,k2);
