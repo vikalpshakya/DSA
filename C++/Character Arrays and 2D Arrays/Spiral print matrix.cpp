581a88edaa74c9578b639a9c45546f728e642fd4
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-08-15 23:36:37 +0530

    Added file

---
+++ C++/Character Arrays and 2D Arrays/Spiral print matrix.cpp
@@ -0,0 +1,32 @@
+void spiralPrint(int **input, int nRows, int nCols)
+{  
+    int top = 0 , buttom = nRows-1 ;
+    int left = 0 , right = nCols-1 ;
+
+    while ( top <= buttom && left <=  right){
+
+        for ( int i = left ; i <= right ; i++){
+            cout << input [top][i] <<" ";
+        }   top ++ ;
+
+
+        for ( int i = top ; i<= buttom;i++){
+            cout << input[i][right] <<" " ;
+        }  right--;
+
+
+        if ( top <= buttom) {
+
+            for ( int i = right ; i >= left ; i--) {
+                cout << input[buttom][i] <<" ";
+            }   buttom-- ;
+        }
+
+        if ( left <=right) {
+
+            for (int i = buttom; i >= top; i--) {
+                cout << input[i][left] <<" ";
+            }   left++;
+        }
+    }
+}
