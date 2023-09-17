a296dc09b2a86822787eedff79f22c7392d4891a
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-09-17 12:36:28 +0530

    Added file

--- C++/Tries and Huffman coding/Search in tries.cpp
+++ C++/Tries and Huffman coding/Search in tries.cpp
@@ -1,1 +1,16 @@
+    bool search_help(TrieNode* root, string word){
+        if(word.size()==0)
+            return root->isTerminal;
+        TrieNode* child;
+        int index = word[0] - 'a';
+        if (root->children[index] != NULL) {
+        child = root->children[index];
+        } 
+        else
+            return false;
+        return search_help(child,  word.substr(1));
+    }
 
+ bool search(string word) {
+        return search_help(root,  word);
+ }
