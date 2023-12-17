6cfb092bc368c3e709622bcc5215ef98a6bf6ce9
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-12-17 17:34:02 +0530

    Added file

--- C++/Graphs/Eventual Safe States gfg.cpp
+++ C++/Graphs/Eventual Safe States gfg.cpp
@@ -4,44 +4,39 @@ class Solution{
 
 // Time complxity: O(V+E), Space complexity: O(n)
 
-private:
-    bool dfs(int n, vector<int> adj[], vector<bool>& visited, vector<bool>& inPath, 
-    vector<bool>& check, int sv){
+class Solution {
+  private:
+    bool dfs(int n, vector<int> adj[], vector<bool>& visited, vector<bool>& inPath, int sv){
         visited[sv] = true;
         inPath[sv] = true;
-        check[sv] = false;
         for(auto i : adj[sv]){
             if(!visited[i]){
-                if(dfs(n,adj,visited,inPath,check,i)){
-                    check[sv] = false;
+                if(dfs(n,adj,visited,inPath,i)){
                     return true;
                 }
             }
             else if(inPath[i]){
-                check[sv] = false;
                 return true;
             }
         }
         
         inPath[sv] = false;
-        check[sv] = true;
         return false;
     }
-    
+     
   public:
     vector<int> eventualSafeNodes(int n, vector<int> adj[]) {
         vector<bool>visited (n,false);
         vector<bool>inPath  (n,false);
-        vector<bool>check   (n,false);
         
         for(int i = 0; i < n; i++){
             if(!visited[i])
-                dfs(n,adj,visited,inPath,check,i);
+                dfs(n,adj,visited,inPath,i);
         }
         
         vector<int>ans;
         for(int i = 0; i < n; i++){
-            if(check[i])
+            if(!inPath[i])
                 ans.push_back(i);
         }
         return ans;
