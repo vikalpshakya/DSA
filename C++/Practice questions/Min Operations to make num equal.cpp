1657670c9ccc989ad3d8eb3c3028f0879725a031
Vikalp Shakya <vikalpshakya@Vikalps-MacBook-Air.local>
2025-06-23 11:28:37 +0530

    new folder created

---
+++ C++/Practice questions/Min Operations to make num equal.cpp
@@ -0,0 +1,42 @@
+// #include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<map>
+
+using namespace std;
+
+const int MAX = 5e6;
+vector<bool>isPrime(MAX+1,true);
+
+void factorize(map<int,int>&mp, int x)  {
+    for(int i = 2; i <= x; i++) {
+        while(x%i == 0) {
+            mp[i]++;
+            x/=i;
+        }
+    }
+}
+
+int main() {
+    int a,b; cin >> a >> b;
+
+    map<int,int>mp1,mp2;
+    // mp1 -> stores factors of a
+    // mp2 -> stores factors of b
+    
+    factorize(mp1,a);
+    factorize(mp2,b);
+
+    int ans = 0;
+
+    for(auto [num,exp] : mp1) {
+        ans += abs(exp - mp2[num]);
+    }
+
+    for(auto [num,exp] : mp2) {
+        if(!mp1[num]) ans += exp;
+    }
+
+    cout << ans << endl;
+
+}
